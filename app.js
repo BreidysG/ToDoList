0308f5b03051889536a308092f7f42d4f617cc4e
BreidysG <breidysgutierrez8@gmail.com>
2023-05-05 11:46:21 -0400

    Funcionalidad de completar y subrayar tarea

--- app.js
+++ app.js
@@ -2,32 +2,59 @@ const date = document.querySelector("#date");
 const list = document.querySelector("#list");
 const input = document.querySelector("#input");
 const buttonEnter = document.querySelector("#enter");
+const lineThrough = "line-through";
+const check = "check";
+let id = 0;
+
 
 /*Function add task*/
 
-function addTask(tarea){
-    const element = `<li>
-                        <span data-="complete" id="0" >✔</span>
-                        <p>${tarea}</p>
-                        <span data-="delete" id="0" >🗑</span>
+function addTask(task, id, complete, del){
+    if(del){return}
+    const COMPLETE = complete? check: "";
+    const LINE = complete? lineThrough: "";
+    const element = `<li id="element">
+                        <span class="${COMPLETE}" data-="complete" id="${id}" >✔</span>
+                        <p class= "text ${LINE}">${task}</p>
+                        <span class="" data-="delete" id="${id}" >🗑</span>
                     </li>`
     list.insertAdjacentHTML("beforeend", element)
 }
 
 buttonEnter.addEventListener("click", ()=>{
-    const tarea = input.value;
-    if(tarea){
-        addTask(tarea);
+    const task = input.value;
+    if(task){
+        addTask(task, id, false, false);
     }
     input.value= "";
+    id ++; 
 });
 
 document.addEventListener("keyup", function(event){
     if(event.key == "Enter"){
-        const tarea = input.value;
-        if(tarea){
-            addTask(tarea);
+        const task = input.value;
+        if(task){
+            addTask(task, id, false, false);
         }
         input.value= "";
+        id ++;
     }
 });
+
+list.addEventListener("click", function(event){
+    const element = event.target;
+    const elementData = element.attributes[1].value;
+
+    if(elementData === "complete"){
+        taskComplete(element);
+    }else if(elementData === "delete"){
+        taskDelete(element);
+    }
+})
+
+/*COMPLETE*/
+
+function taskComplete(element){
+    element.classList.toggle(check); 
+    element.parentNode.querySelector(".text").classList.toggle(lineThrough);
+}
